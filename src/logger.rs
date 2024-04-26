ca44287a8adb76337e6235ec8ad03cac05b02adf
Bo Maryniuk <bo@maryniuk.net>
2024-04-26 17:47:58 +0000

    Change logger format to indicate the service and message level type

--- src/logger.rs
+++ src/logger.rs
@@ -12,16 +12,16 @@ impl log::Log for STDOUTLogger {
         if self.enabled(msg.metadata()) {
             let m = format!("{}", msg.args());
             let l_msg: String = match msg.level() {
-                log::Level::Info => format!("{}", m.bright_green()),
-                log::Level::Warn => format!("{}", m.yellow()),
-                log::Level::Error => format!("{}", m.bright_red()),
-                log::Level::Debug => format!("{}", m.cyan()),
-                log::Level::Trace => format!("{}", m.cyan()),
+                log::Level::Info => format!("I: {}", m.bright_green()),
+                log::Level::Warn => format!("W: {}", m.yellow()),
+                log::Level::Error => format!("E: {}", m.bright_red()),
+                log::Level::Debug => format!("D: {}", m.cyan()),
+                log::Level::Trace => format!("T: {}", m.cyan()),
             };
 
             let tsb = nix::time::clock_gettime(nix::time::ClockId::CLOCK_BOOTTIME);
             let dsb = std::time::Duration::from(tsb.unwrap());
-            println!("[{:12.6}][{:>6}] {}", dsb.as_secs_f32(), format!("T{}", dsb.as_secs()), l_msg);
+            println!("[{:12.6}][{:>6}][Microhop] {}", dsb.as_secs_f32(), format!("T{}", dsb.as_secs()), l_msg);
         }
     }
 
